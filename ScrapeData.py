eae95b8a801ca0a9ee772ac2dabf927de39e96a4
pbuck26 <45185034+pbuck26@users.noreply.github.com>
2020-08-02 02:48:43 -0400

    Add files via upload

--- ScrapeData.py
+++ ScrapeData.py
@@ -1,6 +1,7 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 import datetime
 from datetime import time, timedelta
@@ -12,51 +13,49 @@ import queue
 # build queue for days to be scraped
 q = queue.Queue(maxsize=0)
 
-# I want to grab all of the 2019 season data
-# season starting on march 20
-# and ended September 29
 
 # TODO MAKE THIS CONFIGURABLE ON GUI?
-start = datetime.datetime(2019, 3, 20)
-stop = datetime.datetime(2019, 3, 23)
-index = pd.date_range(start, stop)
-ts = pd.Series()
+#start = datetime.datetime(2019, 3, 20)
+#stop = datetime.datetime(2019, 3, 23)
+#index = pd.date_range(start, stop)
+#ts = pd.Series()
 #q.put(index)
 
-#while loop for scraping    
-for dayIdx in range(len(index)):
-    days = index[dayIdx].strftime("%Y") + "-" + \
-        index[dayIdx].strftime("%m")+ "-" + index[dayIdx].strftime("%d")
-    # Configure webdriver
-    browser = webdriver.Safari()
-    browser.implicitly_wait(6)
-    url = f'https://www.covers.com/sports/mlb/matchups?selectedDate={days}'
-    browser.get(url)
-    # give the browser time to wait
-    time.sleep(3)
-    html = browser.page_source.encode("utf-8")
-    # convert to beautiful soup object
-    soup = bs(html, 'html.parser')
-    currdate = soup.find('a', {'class':'cmg_active_navigation_item'})
-        # print the current day
-    if currdate['data-date'] == days:
-        # temporary data structure is gonna be a list of dicts
-        runTotals = soup.find_all('td', {'class':'cmg_matchup_line_score_total'})
-        teamNames = soup.find_all('div', {'class':'cmg_team_name'})
-        count = 0
-        # find number of scores to associate with this day
-        teamRuns = [None] * len(runTotals)
-        for scores in range(len(runTotals)):
-            count += 1
-            if count ==1:
-                # temporary data structure is gonna be a list of dicts
-                teamRuns[scores] = [{teamNames[scores].text.strip()[-3:]: int(runTotals[scores].text)}]
-            elif count == 2:
-                teamRuns[scores] = [{teamNames[scores].text.strip()[-3:]: int(runTotals[scores].text)}]
-                count = 0
-        print(currdate['data-date'])
-    else:
-        print(f"{days}: no games")
-    browser.quit()
+#Configure webdriver
+browser = webdriver.Safari()
+browser.implicitly_wait(6)
+url = f'https://stats.premierlacrosseleague.com/games'
+browser.get(url)
+# give the browser time to wait
+time.sleep(3)
+html = browser.page_source.encode("utf-8")
+# convert to beautiful soup object
+soup = bs(html, 'html.parser')
+# print the current day
+gameLinks = soup.find_all('a', {'class':'jss15'})
+
+# iterate over each link
+for link in range(len(gameLinks)):
+    try:
+        partialLink = gameLinks[link]['href']
+        browser.get(url[:-6] + partialLink)
+        browser.implicitly_wait(6)
+        # grab all of the team data
+        # table stats --> <td class="MuiTableCell-root MuiTableCell-body jss2409 MuiTableCell-alignCenter MuiTableCell-sizeSmall">9</td>
+        # team name --> <a href="/teams/RED">RED</a>
+        # S	1G	2G	A	Sh	Sh%	SOG	TO	CT	GB	FO	FO%	Sv	Sv%	SA	SAA	PEN	PIM	PP%	PP	PPSh	PK%	PK
+        html = browser.page_source.encode("utf-8")
+        # convert to beautiful soup object
+        soup = bs(html, 'html.parser')
+        teamStats = soup.find_all('td', class_ = 'MuiTableCell-root MuiTableCell-body jss187 MuiTableCell-alignCenter MuiTableCell-sizeSmall')
+        teamNamesTemp = soup.find_all('a', class_ = 'href')
+        TeamNameHome = teamNamesTemp[0].text
+        TeamNameAway = teamNamesTemp[1].text
+    except:
+        print("Link Invalid")
+        continue
     
 
+
+
+browser.quit()
